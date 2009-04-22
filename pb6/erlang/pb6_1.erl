ce3aca71aed32e94a827b9e03cad7be0a9dc0d48
Laurent Laborde <kerdezixe@gmail.com>
2009-04-22 21:29:37 +0200

    First commit of pb6 with erlang solution

---
+++ pb6/erlang/pb6_1.erl
@@ -0,0 +1,20 @@
+% The sum of the squares of the first ten natural numbers is,
+% 1^(2) + 2^(2) + ... + 10^(2) = 385
+% The square of the sum of the first ten natural numbers is,
+% (1 + 2 + ... + 10)^(2) = 55^(2) = 3025
+%
+% Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+% Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+-module(pb6_1).
+-export([start/0]).
+
+
+start() ->
+	compute(100).
+
+compute(N) ->
+	SumOfSquare = lists:sum([X*X || X <- lists:seq(1,N)]),
+	Sum = lists:sum(lists:seq(1,N)),
+	Result =  (Sum*Sum) - SumOfSquare.
+
